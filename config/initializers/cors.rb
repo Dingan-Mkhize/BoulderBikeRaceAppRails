7620256758dfdc9ee51dc9e0efae432c1a84dc46
Dingan Mkhize <dingan.mkhize@yahoo.com>
2024-07-31 14:42:15 +0100

    url update in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'project7-bike-race-site-production.up.railway.app', 'localhost:3001'
+    origins 'https://project7-bike-race-site-production.up.railway.app'
     resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
   end
 end
