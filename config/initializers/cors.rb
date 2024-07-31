a087d99081c2926bedd9775d4de2c3c99b5682b9
Dingan Mkhize <dingan.mkhize@yahoo.com>
2024-07-31 14:14:53 +0100

    cors pointed to react frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001' # Update with the actual URL of your React app
-    resource '*', headers: :any, methods: [:get, :post, :options]
+    origins 'project7-bike-race-site-production.up.railway.app', 'localhost:3001'
+    resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
   end
 end
